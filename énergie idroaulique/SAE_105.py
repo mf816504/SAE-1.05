17972c08fc36b93a8abab63a807c7efc020586b1
af526009 <af526009@iut-dijon.u-bourgogne.fr>
2026-01-08 16:53:54 +0100

    Add files via upload

---
+++ énergie idroaulique/SAE_105.py
@@ -0,0 +1,26 @@
+import csv
+import matplotlib.pyplot as plt
+
+annees = []
+productions = []
+
+with open("hydraulique.csv", newline='', encoding="utf-8") as fichier:
+    lecteur = csv.reader(fichier, delimiter=';')
+    next(lecteur)
+
+    for ligne in lecteur:
+        annee = int(ligne[0][:4])
+        valeur = float(ligne[2].replace(',', '.'))
+
+        if annee in annees:
+            index = annees.index(annee)
+            productions[index] = productions[index] + valeur
+        else:
+            annees.append(annee)
+            productions.append(valeur)
+
+plt.bar(annees, productions)
+plt.title("Évolution de la production d'électricité hydraulique")
+plt.xlabel("Années")
+plt.ylabel("Production annuelle en TWh")
+plt.show()
