0583b0cdf5f878fc0ba4b486434310a30f163b16
af526009 <af526009@iut-dijon.u-bourgogne.fr>
2026-01-19 11:51:16 +0100

    Energie_Thermique.py

--- énergie thermique fossile/thermique.py
+++ énergie thermique fossile/thermique.py
@@ -1,27 +1,29 @@
-import csv
-import matplotlib.pyplot as plt
+import csv #Import du module
+import matplotlib.pyplot as plt #Import du module
 
-annees = []
-productions = []
+annees = []  #creation de la liste qui va servir à stocker les années
+productions = [] #creation de la liste qui va servir à stocker la production total
 
-with open("thermique.csv", newline='', encoding="utf-8") as fichier:
-    lecteur = csv.reader(fichier, delimiter=';')
-    next(lecteur)
+with open("thermique.csv", newline='', encoding="utf-8") as fichier:  # Ouverture du fichier csv pour pouvoir le lire 
+    lecteur = csv.reader(fichier, delimiter=';') # Déclaration du délimiteur
+    next(lecteur) # Saute la première ligne pour la bonne lecture des données
 
-    for ligne in lecteur:
-        if ligne and ligne[2] != '':
-            annee = int(ligne[0][:4])
-            valeur = float(ligne[2].replace(',', '.'))
+    for ligne in lecteur: #Parcour le fichier
+        
+        if ligne and ligne[2] != '': #Vérifie si la ligne est vide
+            annee = int(ligne[0][:4])  # Extrait l'année du fichier
+            valeur = float(ligne[2].replace(',', '.')) # La valeur de production est récupérée, convertie en nombre réel et la virgule est remplacée par un point
 
-            if annee in annees:
-                index = annees.index(annee)
-                productions[index] = productions[index] + valeur
+            if annee in annees:  #Parcour le fichier
+                index = annees.index(annee) # Récupèrer l'index de l'année
+                productions[index] +=valeur
             else:
-                annees.append(annee)
-                productions.append(valeur)
+                annees.append(annee) #Complète la liste avec les valeurs
+                productions.append(valeur) #Complète la liste avec les valeurs
 
-plt.bar(annees, productions)
-plt.title("Évolution de la production d'électricité thermique fossile")
-plt.xlabel("Années")
-plt.ylabel("Production annuelle en TWh")
-plt.show()
+plt.bar(annees, productions) # Produit un diagramme en bar
+plt.title("Évolution de la production d'électricité thermique fossile") # Ajoute un titre
+plt.xlabel("Années") #Nom des axes
+plt.ylabel("Production annuelle en TWh") #Nom des axes
+plt.show() # Affiche le graphique 
+
