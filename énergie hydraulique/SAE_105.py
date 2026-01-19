fe4d0267ff96dbce9eb12bbb19ffca0f93456bc3
af526009 <af526009@iut-dijon.u-bourgogne.fr>
2026-01-19 11:24:44 +0100

    Energie_hydraulique.py

--- énergie hydraulique/SAE_105.py
+++ énergie hydraulique/SAE_105.py
@@ -1,26 +1,27 @@
-import csv
-import matplotlib.pyplot as plt
+import csv #Import du module
+import matplotlib.pyplot as plt #Import du module
 
-annees = []
-productions = []
+annees = [] #creation de la liste qui va servir à stocker les années
+productions = [] #creation de la liste qui va servir à stocker la production total
 
-with open("hydraulique.csv", newline='', encoding="utf-8") as fichier:
-    lecteur = csv.reader(fichier, delimiter=';')
-    next(lecteur)
+with open("hydraulique.csv", newline='', encoding="utf-8") as fichier: # Ouverture du fichier csv pour pouvoir le lire 
+    lecteur = csv.reader(fichier, delimiter=';') # Déclaration du délimiteur
+    next(lecteur) # Saute la première ligne pour la bonne lecture des données
 
-    for ligne in lecteur:
-        annee = int(ligne[0][:4])
-        valeur = float(ligne[2].replace(',', '.'))
+    for ligne in lecteur: #Parcour le fichier ligne par ligne
+        annee = int(ligne[0][:4]) # Extrait l'année du fichier 
+        valeur = float(ligne[2].replace(',', '.'))  # La valeur de production est récupérée, convertie en nombre réel et la virgule est remplacée par un point
 
-        if annee in annees:
-            index = annees.index(annee)
-            productions[index] = productions[index] + valeur
+        if annee in annees: #Parcour le fichier
+            index = annees.index(annee) # Récupèrer l'index de l'année
+            productions[index] += valeur
         else:
-            annees.append(annee)
-            productions.append(valeur)
+            annees.append(annee)  #Complète la liste avec les valeurs
+            productions.append(valeur)  #Complète la liste avec les valeurs
 
-plt.bar(annees, productions)
-plt.title("Évolution de la production d'électricité hydraulique")
-plt.xlabel("Années")
-plt.ylabel("Production annuelle en TWh")
-plt.show()
+plt.bar(annees, productions) # Produit un diagramme en bar
+plt.title("Évolution de la production d'électricité hydraulique") # Ajoute un titre
+plt.xlabel("Années") #Nom des axes
+plt.ylabel("Production annuelle en TWh") #Nom des axes
+plt.show() # Affiche le graphique 
+
